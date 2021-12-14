551446c86a6c750bbce9528f759dca9f884a09d9
jakobottar <jakobottar@pm.me>
2021-12-14 16:01:00 -0700

    added svm, beat score

--- svm.py
+++ svm.py
@@ -2,11 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 # import helper
+
+import warnings
+warnings.filterwarnings("ignore", message="X does not have valid feature names, but AdaBoostClassifier was fitted with feature names")
 
 print("loading training data...")
 raw_train = pd.read_csv("./data/train.csv")
@@ -14,15 +17,12 @@ raw_train = pd.read_csv("./data/train.csv")
 # got info on categorical data handling from 
 # https://analyticsindiamag.com/complete-guide-to-handling-categorical-data-using-scikit-learn/
 
-train = raw_train.copy()
-# train.drop('education.num', axis=1, inplace=True) # I drop this because I want to do the conversion myself
-# train.drop('native.country', axis=1, inplace=True)
-
 # determine categorical variables
-s = (train.dtypes == 'object')
+s = (raw_train.dtypes == 'object')
 object_cols = list(s[s].index)
 
 le = LabelEncoder()
+train = raw_train.copy()
 
 # LabelEncode the categorical variables
 for col in object_cols:
@@ -33,46 +33,27 @@ for col in object_cols:
 Xs_train = train.drop('income>50K', axis=1)
 ys_train = train['income>50K']
 
-# create Random Forest Classifier and do 5-fold CV
+weights = {0 : 0.25,
+           1 : 0.75}
+
+# create AdaBoost Classifier and do 5-fold CV
 print("5-fold cross-validation...")
-xs = np.arange(0, 810, 10)
-xs[0] = 1
-scores = []
-max_score = -1
-best_x = 0
+svc = make_pipeline(StandardScaler(), SVC(class_weight='balanced'))
+print(svc)
+cv_acc = cross_val_score(svc, Xs_train, ys_train, cv=5, n_jobs=-1)
+print(cv_acc.mean())
 
-print(Xs_train)
-
-for x in xs:
-    clf = make_pipeline(StandardScaler(),RandomForestClassifier(n_estimators=x))
-    cv_acc = cross_val_score(clf, Xs_train, ys_train, cv=5, n_jobs=-1)
-    scores.append(cv_acc.mean())
-    print(f"{x}: {cv_acc.mean():>4f}")
-    if cv_acc.mean() > max_score: 
-        max_score = cv_acc.mean()
-        best_x = x
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.plot(xs, scores, color = 'tab:orange', label = "training accuracy")
-ax.legend()
-ax.set_xlabel("# of estimators")
-ax.set_ylabel("Accuracy")
-
-plt.savefig("./reports/img/rf_acc.png")
-
-# train on full training dataset
-print(f"training final version with {best_x} classifiers...")
-clf = RandomForestClassifier(n_estimators=best_x)
-clf = clf.fit(Xs_train, ys_train)
-print(clf.feature_importances_)
+print("training on full dataset...")
+svc_final = make_pipeline(StandardScaler(), SVC(class_weight='balanced'))
+svc_final = svc_final.fit(Xs_train, ys_train)
 
 ### Make Submission from test.csv
 print("loading testing data...")
 raw_test = pd.read_csv("./data/test.csv")
 
 test = raw_test.copy()
-# test.drop(['education.num', 'ID'], axis=1, inplace=True)
+
+test.drop('ID', axis=1, inplace=True)
 
 # LabelEncode the categorical variables
 for col in object_cols:
@@ -81,10 +62,11 @@ for col in object_cols:
 
 #predict resulting values
 print("predicting from testing data...")
-pred_test = clf.predict(test)
+pred_test = svc_final.predict(test)
 
 data = {'ID': list(range(1, len(pred_test)+1)),
         'Prediction': pred_test}
 out = pd.DataFrame(data)
 
-out.to_csv("./submissions/rf_submit.csv", index=False)
+out.to_csv("./submissions/svm_submit.csv", index=False)
+print("Done!")
